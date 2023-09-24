2dbd3a2579a99d609b53822f10a34114e563e883
Saswata Alex Majumder <majumder.ua@gmail.com>
2023-09-24 01:05:37 -0400

    arithmetic lab done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
+const num1 = 15.5;
+const num2 = 4;
+const num3 = 24; 
+const num4 = 5;
 // Write your code here
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 99);
+
+let mod = num3 % num4;
+
+let max = Math.max (1,3,6,9,11,20,4);
